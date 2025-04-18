94c3baddce6ddc55d8fff5e1cb9fc093755333ce
hattori_matsuzakilab <1424521@ed.tus.ac.jp>
2025-04-18 13:36:09 +0900

    [WIP] VkInstanceの生成機構を追加

--- Vulkan/header/Core/vulkan_core.h
+++ Vulkan/header/Core/vulkan_core.h
@@ -6,8 +6,12 @@
 * @date 2025/4/17
 */
 
+#include<vector>
+
 // ウィンドウ生成に関する各種機能を宣言するヘッダー
-#define GLFW_INCLUDE_VULKAN
+#define GLFW_INCLUDE_VULKAN 1
+// Vulkanの各種関数や構造体、列挙型を提供してくれるヘッダー
+#include <vulkan/vulkan.h>
 #include <GLFW/glfw3.h>
 
 namespace Core
@@ -29,6 +33,11 @@ namespace Core
 		* @brief GLFWのウィンドウ設定を初期化する
 		*/
 		void InitWindow();
+
+		/**
+		* @brief VkInstanceを生成する。
+		*/
+		void CreateVkInstance();
 		/**
 		* @fn
 		* @brief Vulkanの各種コンポーネントを初期化する。
@@ -45,7 +54,34 @@ namespace Core
 		*/
 		void CleanUp();
 
+		/**
+		* @fn
+		* @brief Vulkan アプリケーションの基本情報を設定する
+		*/
+		void CreateVkApplicationInfo(VkApplicationInfo& info);
+
+		/**
+		* @fn
+		* @brief VkInstanceの生成設定を行う
+		* @detail デバイスにかかわらず全てのプログラムに適用する拡張(extensions)と使用するバリデーションレイヤの設定を行う。
+		*/
+		void CreateVkInstanceCreateInfo(VkInstanceCreateInfo& create_info, const VkApplicationInfo& application_info);
+		
+		/**
+		* @fn
+		* @brief
+		* 要求されている拡張機能が全てサポートされているかをチェックする
+		* @detail
+		* VkInstance生成時にVkInstanceCreateInfo内で設定している必要な拡張機能がサポートされていない場合、
+		* VK_ERROR_EXTENSION_NOT_PRESENTエラーコードが発生する。
+		* これに未然に対応するために、vkEnumerateInstanceExtensionProperties関数でサポートされている拡張機能を取得し、
+		* 必要な拡張機能がサポートされているかを事前チェックする。
+		* 
+		*/
+		bool CheckExtensionsAvailable(std::vector<const char*> required_instance);
+
 	private:
 		GLFWwindow* window_;
+		VkInstance instance_;
 	};
 }
--- Vulkan/src/Core/vulkan_core.cpp
+++ Vulkan/src/Core/vulkan_core.cpp
@@ -1,7 +1,7 @@
 #include "Core/vulkan_core.h"
 
-// Vulkanの各種関数や構造体、列挙型を提供してくれるヘッダー
-#include <vulkan/vulkan.h>
+#include<stdexcept>
+#include<iostream>
 
 namespace Core {
 	void VulkanApplication::InitWindow() {
@@ -15,7 +15,7 @@ namespace Core {
 	}
 
 	void VulkanApplication::InitVulkan() {
-
+		CreateVkInstance();
 	}
 	void VulkanApplication::MainLoop() {
 		// エラーが出るまではウィンドウに対するイベントを観察し続ける
@@ -25,6 +25,7 @@ namespace Core {
 	}
 
 	void VulkanApplication::CleanUp() {
+		vkDestroyInstance(instance_, nullptr);
 		glfwDestroyWindow(window_);
 		glfwTerminate();
 	}
@@ -34,5 +35,92 @@ namespace Core {
 		InitVulkan();
 		MainLoop();
 		CleanUp();
+	}
+
+	void VulkanApplication::CreateVkInstance() {
+		VkApplicationInfo app_info{};
+		CreateVkApplicationInfo(app_info);
+		VkInstanceCreateInfo create_info{};
+		CreateVkInstanceCreateInfo(create_info, app_info);
+		// 二番目の引数はカスタムアロケーターへのポインタ
+		if (vkCreateInstance(&create_info, nullptr, &instance_) != VK_SUCCESS) {
+			throw std::runtime_error("VkInstanceの生成に失敗しました！");
+		}
+	}
+
+	/**
+	* @fn
+	* @brief Vulkan アプリケーションの基本情報を設定する
+	*/
+	void VulkanApplication::CreateVkApplicationInfo(VkApplicationInfo& info) {
+		// Vulkanでは構造体のsTypeメンバーで明示的に構造体の型を設定する必要がある。
+		// pNext メンバで拡張情報を設定することもできるが、とりあえずnullptrのままにする
+		info.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
+		info.pApplicationName = "Triangle";
+		info.applicationVersion = VK_MAKE_VERSION(1, 0, 0);
+		info.pEngineName = "No Engine";
+		info.engineVersion = VK_MAKE_VERSION(1, 0, 0);
+		info.apiVersion = VK_API_VERSION_1_0;
+	}
+
+	/**
+	* @fn
+	* @brief VkInstanceの生成設定を行う
+	* @detail デバイスにかかわらず全てのプログラムに適用する拡張(extensions)と使用するバリデーションレイヤの設定を行う。
+	*/
+	void VulkanApplication::CreateVkInstanceCreateInfo(VkInstanceCreateInfo& create_info, const VkApplicationInfo& application_info) {
+		create_info.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+		create_info.pApplicationInfo = &application_info;
+
+		// global extension の設定
+		// Vulkanはプラットフォーム非依存であるため、ウィンドウシステムと接続するには拡張機能が必要
+		uint32_t glfw_extension_count = 0;
+		// 文字列の配列なのでポインタのポインタになる
+		const char** glfw_extensions;
+
+		glfw_extensions = glfwGetRequiredInstanceExtensions(&glfw_extension_count);
+
+		uint32_t extension_count = 0;
+		vkEnumerateInstanceExtensionProperties(nullptr, &extension_count, nullptr);
+		// MacOSではVK_ERROR_INCOMPATIBLE_DRIVERエラーが発生する
+		// これに対応するには、VK_KHR_PORTABILITY_subset 拡張を追加する必要がある。
+		//std::vector<const char*> required_extensions;
+		//for (uint32_t index = 0; index < glfw_extension_count; index++) {
+		//	required_extensions.emplace_back(glfw_extensions[index]);
+		//}
+		//required_extensions.emplace_back(VK_KHR_PORTABILITY_ENUMERATION_EXTENSION_NAME);
+
+		//create_info.flags |= VK_INSTANCE_CREATE_ENUMERATE_PORTABILITY_BIT_KHR;
+
+		//if (!CheckExtensionsAvailable(required_extensions)) {
+		//	throw std::runtime_error("拡張機能がサポートされていません！");
+		//}
+		//create_info.enabledExtensionCount = (uint32_t)required_extensions.size();
+		//create_info.ppEnabledExtensionNames = required_extensions.data();
+		create_info.enabledExtensionCount = glfw_extension_count;
+		create_info.ppEnabledExtensionNames = glfw_extensions;
+
+		// バリデーションレイヤの設定
+		create_info.enabledLayerCount = 0;
+	}
+
+	bool VulkanApplication::CheckExtensionsAvailable(std::vector<const char*> required_instance) {
+		uint32_t extension_count = 0;
+		vkEnumerateInstanceExtensionProperties(nullptr, &extension_count, nullptr);
+		// 要素数を取得してから拡張機能配列を用意して名前を取得
+		std::vector<VkExtensionProperties> extensions(extension_count);
+		vkEnumerateInstanceExtensionProperties(nullptr, &extension_count, extensions.data());
+		// 拡張機能名の配列を作る
+		std::vector<const char*> extension_names;
+		for (const auto& property : extensions) {
+			extension_names.emplace_back(property.extensionName);
+		}
+		// 含まれているかどうかを確認する
+		for (const auto& instance : required_instance) {
+			if (std::find(extension_names.begin(), extension_names.end(), instance) == extension_names.end()) {
+				return false;
+			}
+		}
+		return true;
 	}
 }
