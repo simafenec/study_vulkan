e8abdccd0c08fd3521a139bf1f960ea9109ce549
hattori_matsuzakilab <1424521@ed.tus.ac.jp>
2025-04-20 22:43:57 +0900

    feat : バリデーションレイヤの追加

--- Vulkan/header/Core/vulkan_core.h
+++ Vulkan/header/Core/vulkan_core.h
@@ -7,6 +7,7 @@
 */
 
 #include<vector>
+#include<iostream>
 
 // ウィンドウ生成に関する各種機能を宣言するヘッダー
 #define GLFW_INCLUDE_VULKAN 1
@@ -18,8 +19,17 @@ namespace Core
 {
 	class VulkanApplication {
 	public:
-		const uint32_t kWIDTH = 800;
-		const uint32_t kHEIGHT = 600;
+		const uint32_t kWidth = 800;
+		const uint32_t kHeight = 600;
+	private:
+		const std::vector<const char*> kValidationLayers = {
+			"VK_LAYER_KHRONOS_validation"
+		};
+#ifdef NDEBUG
+		const bool kEnableValidationLayers = false;
+#else
+		const bool kEnableValidationLayers = true;
+#endif
 
 	public:
 		/**
@@ -71,10 +81,80 @@ namespace Core
 		* @fn
 		* @brief 必要な拡張機能のリストを取得する。
 		*/
-		std::vector<const char*> getRequiredExtensions();
+		std::vector<const char*> GetRequiredExtensions();
+
+		/**
+		* @fn
+		* @brief
+		* バリデーションレイヤがサポートされているかどうかを確認する。
+		*/
+		bool CheckValidationLayerSupport();
+
+		/**
+		* @fn
+		* @brief
+		* デバッグメッセンジャーの生成情報を初期化する
+		*/
+		void InitializeDebugMessengerCreateInfo(
+			VkDebugUtilsMessengerCreateInfoEXT& create_info				// デバッグメッセンジャー生成情報インスタンスへの参照
+		);
+		/**
+		* @fn
+		* @brief
+		* デバッグメッセージの通知システムをセットアップする
+		*/
+		void SetupDebugMessenger();
+
+		/**
+		* @fn
+		* @brief 
+		* デバッグメッセンジャーを生成する
+		*/
+		VkResult CreateDebugUtilsMessenger(
+			const VkDebugUtilsMessengerCreateInfoEXT* p_create_info,	// デバッグメッセンジャーの生成情報
+			const VkAllocationCallbacks* p_allocator					// カスタムアロケーターへのポインタ
+		);
+
+		/**
+		* @fn
+		* @brief
+		* デバッグメッセンジャーを破棄する
+		*/
+		void DestroyDebugUtilsMessenger(
+			VkAllocationCallbacks* p_allocator
+		);
+
+		/**
+		* @fn
+		* @brief
+		* デバッグメッセージのコールバック関数
+		* @return
+		* Validation Layer をトリガした関数の処理をabortすべきかどうか
+		*/
+		static VKAPI_ATTR VkBool32 VKAPI_CALL DebugCallBack(
+			VkDebugUtilsMessageSeverityFlagBitsEXT message_severity,	 // デバッグメッセージの重要度
+			VkDebugUtilsMessageTypeFlagsEXT message_type,				 // 発生したメッセージの種類
+			const VkDebugUtilsMessengerCallbackDataEXT* p_callback_data, // メッセージの詳細な内容を含む構造体
+			void* p_user_data											 // コールバックのセットアップ時に与えられる独自データ
+		) {
+			// VK_DEBUG_UTILS_MESSAGE_SEVERITY_VERBOSE_BIT_EXT: 診断メッセージ
+			// VK_DEBUG_UTILS_MESSAGE_SEVERITY_INFO_BIT_EXT: リソースメッセージなどの情報提供メッセージ
+			// VK_DEBUG_UTILS_MESSAGE_SEVERITY_WARNING_BIT_EXT : エラーではないがバグに近い警告メッセージ
+			// VK_DEBUG_UTILS_MESSAGE_SEVERITY_ERROR_BIT_EXT : クラッシュを引き起こしうる不正な動きを通知するメッセージ
+			// 下に行けば行くほど重要度大 (割り当てられた値も大きい)
+			if (message_severity >= VK_DEBUG_UTILS_MESSAGE_SEVERITY_WARNING_BIT_EXT) {
+				std::cerr << "validation layer : " << p_callback_data->pMessage << std::endl;
+				
+			}
+			else {
+				std::cout << "validation layer : " << p_callback_data->pMessage << std::endl;
+			}
+			return VK_FALSE;
+		}
 
 	private:
 		GLFWwindow* window_;
 		VkInstance instance_;
+		VkDebugUtilsMessengerEXT debug_messenger_;
 	};
 }
--- Vulkan/src/Core/vulkan_core.cpp
+++ Vulkan/src/Core/vulkan_core.cpp
@@ -1,7 +1,6 @@
 #include "Core/vulkan_core.h"
 
 #include<stdexcept>
-#include<iostream>
 
 namespace Core {
 	void VulkanApplication::InitWindow() {
@@ -11,11 +10,12 @@ namespace Core {
 		// ウィンドウのリサイズは特別な対応が必要になるためいったんできないようにする
 		glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
 		// 四つ目のパラメータは画面を開くモニターを指定でき、五つ目のパラメータはOpenGL以外では必要ない
-		window_ = glfwCreateWindow(kWIDTH, kHEIGHT, "Vulkan", nullptr, nullptr);
+		window_ = glfwCreateWindow(kWidth, kHeight, "Vulkan", nullptr, nullptr);
 	}
 
 	void VulkanApplication::InitVulkan() {
 		CreateVkInstance();
+		SetupDebugMessenger();
 	}
 	void VulkanApplication::MainLoop() {
 		// エラーが出るまではウィンドウに対するイベントを観察し続ける
@@ -25,6 +25,9 @@ namespace Core {
 	}
 
 	void VulkanApplication::CleanUp() {
+		if (kEnableValidationLayers) {
+			DestroyDebugUtilsMessenger(nullptr);
+		}
 		vkDestroyInstance(instance_, nullptr);
 		glfwDestroyWindow(window_);
 		glfwTerminate();
@@ -38,6 +41,9 @@ namespace Core {
 	}
 
 	void VulkanApplication::CreateVkInstance() {
+		if (kEnableValidationLayers && !CheckValidationLayerSupport()) {
+			throw std::runtime_error("バリデーションレイヤーの設定が求められましたが、利用可能ではありません");
+		}
 		VkApplicationInfo app_info{};
 		// Vulkanでは構造体のsTypeメンバーで明示的に構造体の型を設定する必要がある。
 		// pNext メンバで拡張情報を設定することもできるが、とりあえずnullptrのままにする
@@ -51,7 +57,7 @@ namespace Core {
 		VkInstanceCreateInfo create_info{};
 		create_info.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
 		create_info.pApplicationInfo = &app_info;
-		auto extensions = getRequiredExtensions();
+		auto extensions = GetRequiredExtensions();
 		if (!CheckExtensionsAvailable(extensions)) {
 			throw std::runtime_error("拡張機能がサポートされていません！");
 		}
@@ -59,8 +65,23 @@ namespace Core {
 		create_info.flags |= VK_INSTANCE_CREATE_ENUMERATE_PORTABILITY_BIT_KHR;
 		create_info.enabledExtensionCount = static_cast<uint32_t>(extensions.size());
 		create_info.ppEnabledExtensionNames = extensions.data();
+
+		// インスタンス生成 / 破棄時のエラーデバッグ対応
+		// インスタンス生成時の pNextにデバッグ情報を与える
+		VkDebugUtilsMessengerCreateInfoEXT debug_create_info{};
 		// バリデーションレイヤの設定
-		create_info.enabledLayerCount = 0;
+		if (kEnableValidationLayers) {
+			create_info.enabledLayerCount = static_cast<uint32_t>(kValidationLayers.size());
+			create_info.ppEnabledLayerNames = kValidationLayers.data();
+
+			// インスタンス生成時のバリデーション情報設定
+			InitializeDebugMessengerCreateInfo(debug_create_info);
+			create_info.pNext = (VkDebugUtilsMessengerCreateInfoEXT*)&debug_create_info;
+		}
+		else {
+			create_info.enabledLayerCount = 0;
+			create_info.pNext = nullptr;
+		}
 		// 二番目の引数はカスタムアロケーターへのポインタ
 		VkResult result = vkCreateInstance(&create_info, nullptr, &instance_);
 		if (result != VK_SUCCESS) {
@@ -91,7 +112,7 @@ namespace Core {
 		return true;
 	}
 
-	std::vector<const char*>  VulkanApplication::getRequiredExtensions() {
+	std::vector<const char*>  VulkanApplication::GetRequiredExtensions() {
 		// global extension の設定
 		// Vulkanはプラットフォーム非依存であるため、ウィンドウシステムと接続するには拡張機能が必要
 		uint32_t glfw_extension_count = 0;
@@ -103,7 +124,84 @@ namespace Core {
 		// これに対応するには、VK_KHR_PORTABILITY_subset 拡張を追加する必要がある。
 		std::vector<const char*> extensions(glfw_extensions, glfw_extensions + glfw_extension_count);
 		extensions.push_back(VK_KHR_PORTABILITY_ENUMERATION_EXTENSION_NAME);
-
+		if (kEnableValidationLayers) {
+			extensions.push_back(VK_EXT_DEBUG_UTILS_EXTENSION_NAME);
+		}
 		return extensions;
+	}
+
+	bool VulkanApplication::CheckValidationLayerSupport() {
+		// 使用可能なレイヤーをすべて取得
+		uint32_t layer_count;
+		vkEnumerateInstanceLayerProperties(&layer_count, nullptr);
+
+		std::vector<VkLayerProperties> available_layers(layer_count);
+		vkEnumerateInstanceLayerProperties(&layer_count, available_layers.data());
+
+		// バリデーションレイヤーが使用可能かどうか確認
+		for (const char* layer_name : kValidationLayers) {
+			bool layer_found = false;
+			for (const VkLayerProperties& prop : available_layers) {
+				if (strcmp(layer_name, prop.layerName) == 0) {
+					layer_found = true;
+					break;
+				}
+			}
+			if (!layer_found) {
+				return false;
+			}
+		}
+		return true;
+	}
+	void VulkanApplication::InitializeDebugMessengerCreateInfo(
+		VkDebugUtilsMessengerCreateInfoEXT& create_info				// デバッグメッセンジャー生成情報インスタンスへの参照
+	) {
+		create_info = {};
+		create_info.sType = VK_STRUCTURE_TYPE_DEBUG_UTILS_MESSENGER_CREATE_INFO_EXT;
+		// フラグはコールバックを呼び出すメッセージの種類を設定する
+		create_info.messageSeverity = VK_DEBUG_UTILS_MESSAGE_SEVERITY_VERBOSE_BIT_EXT
+			| VK_DEBUG_UTILS_MESSAGE_SEVERITY_WARNING_BIT_EXT
+			| VK_DEBUG_UTILS_MESSAGE_SEVERITY_ERROR_BIT_EXT;
+		create_info.messageType = VK_DEBUG_UTILS_MESSAGE_TYPE_GENERAL_BIT_EXT
+			| VK_DEBUG_UTILS_MESSAGE_TYPE_VALIDATION_BIT_EXT
+			| VK_DEBUG_UTILS_MESSAGE_TYPE_PERFORMANCE_BIT_EXT;
+		create_info.pfnUserCallback = DebugCallBack;
+		create_info.pUserData = nullptr;
+	}
+
+	void VulkanApplication::SetupDebugMessenger() {
+		if (!kEnableValidationLayers) {
+			return;
+		}
+		VkDebugUtilsMessengerCreateInfoEXT create_info;
+		InitializeDebugMessengerCreateInfo(create_info);
+
+		if (CreateDebugUtilsMessenger(&create_info, nullptr) != VK_SUCCESS) {
+			throw std::runtime_error("デバッグメッセンジャーの設定に失敗しました。");
+		}
+	}
+
+	VkResult VulkanApplication::CreateDebugUtilsMessenger(
+		const VkDebugUtilsMessengerCreateInfoEXT* p_create_info,	// デバッグメッセンジャーの生成情報
+		const VkAllocationCallbacks* p_allocator					// カスタムアロケーターへのポインタ
+	) {
+		// デバッグメッセンジャーを生成する関数は拡張機能であるためデフォルトでロードされない
+		// vkGetInstanceProcAddr 関数を使って関数のポインタを検索する必要がある。
+		auto proc = (PFN_vkCreateDebugUtilsMessengerEXT)vkGetInstanceProcAddr(instance_, "vkCreateDebugUtilsMessengerEXT");
+		if (proc != nullptr) {
+			return proc(instance_, p_create_info, p_allocator, &debug_messenger_);
+		}
+		else {
+			return VK_ERROR_EXTENSION_NOT_PRESENT;
+		}
+	}
+
+	void VulkanApplication::DestroyDebugUtilsMessenger(
+		VkAllocationCallbacks* p_allocator
+	) {
+		auto proc = (PFN_vkDestroyDebugUtilsMessengerEXT)vkGetInstanceProcAddr(instance_, "vkDestroyDebugUtilsMessengerEXT");
+		if (proc != nullptr) {
+			proc(instance_, debug_messenger_, p_allocator);
+		}
 	}
 }
