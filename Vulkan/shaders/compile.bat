198ae04accd509b0f12aeab3607dff434390e0f7
simafenec <simacafe0823@gmail.com>
2026-05-06 19:11:22 +0900

    feat : パーティクル用のコンピュートシェーダーを追加

--- Vulkan/shaders/compile.bat
+++ Vulkan/shaders/compile.bat
@@ -1,3 +1,4 @@
 C:/VulkanSDK/1.4.309.0/Bin/glslc.exe shader.vert -o vert.spv
 C:/VulkanSDK/1.4.309.0/Bin/glslc.exe shader.frag -o frag.spv
+C:/VulkanSDK/1.4.309.0/Bin/glslc.exe particle_compute.comp -o particle_compute.spv
 pause
